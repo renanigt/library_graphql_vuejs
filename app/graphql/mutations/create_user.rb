2b7c5db741a94701302fc00bed20b8135f5d5995
renanigt <renanigt@gmail.com>
2020-11-12 00:41:12 -0300

    creates user type and mutation

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,12 @@
+module Mutations
+  class CreateUser < BaseMutation
+    argument :email, String, required: true
+    argument :password, String, required: true
+
+    type Types::UserType
+
+    def resolve(email:, password:)
+      User.create!(email: email, password_digest: password)
+    end
+  end
+end
